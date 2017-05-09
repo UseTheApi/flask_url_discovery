c6acf85ef8f576d5a276b98c7abbd07f4645db15
usetheapi <lifar_tut_net@mail.ru>
2017-05-09 19:22:48 +0300

    [feat] private() method and decorator for view_functions and blueprints

---
+++ flask_url_discovery/urls_privation.py
@@ -0,0 +1,63 @@
+#! /bin/usr/python3
+
+__author__ = "Alena Lifar"
+__email__ = "alenaslifar@gmail.com"
+__date__ = "05/08/2017"
+
+private_view_functions = list()
+private_blueprints = list()
+
+
+def add_private_link(func):
+    """
+    Appends func to private collection
+    :param func: decorated function
+    :return: void
+    """
+    global private_view_functions
+    private_view_functions.append(func)
+
+
+def add_private_bp(bp):
+    """
+    Adds a Blueprint name into collection
+    :param bp: flask Blueprint
+    :return:
+    """
+    global private_blueprints
+    private_blueprints.append(bp)
+
+
+def private(bp=None):
+    """
+    Function and Decorator that allows a user to private some urls and blueprints
+    Usage:
+        with routes:
+
+    @private()
+    @app.route("/route/", methods=["GET"], **options)
+    def view_func():
+        ...
+
+        with blueprints:
+
+    some_bp = private(Blueprint("name", __name__))
+
+    or
+
+    some_bp = Blueprint("name", __name__)
+    private(some_bp)
+
+    private does not modify route or blueprint
+
+    :param bp: flask Blueprint
+    :return: func
+    """
+    if bp:
+        add_private_bp(bp)
+        return bp
+
+    def decorator(func):
+        add_private_link(func)
+        return
+    return decorator
---
+++ tests/private_urls_test.py
@@ -0,0 +1,75 @@
+#! /bin/usr/python3
+
+from .test_base import TestBase
+from ..flask_url_discovery.urls_privation import private
+from ..flask_url_discovery.app_registry import discover_urls
+
+app1_simple_case_links = ['test_route1', 'static']
+
+
+class TestPrivateUrls(TestBase):
+
+    def setUp(self):
+        super().setUp()
+
+    def test_PrivateLink(self):
+        """
+        Adds a private rule to an app.
+        Validates that the added rule was hidden and not exposed
+        :return: void
+        """
+        print()
+        print("====== TEST PRIVATE LINK ======")
+
+        @private()
+        @self.app1.route("/test/hidden_rule/")
+        def hidden_rule():
+            """
+            Test url_rule + view_function
+            :return: void
+            """
+            return
+
+        discover_urls(self.app1)
+        print("Collected Links: ", self.app1.ud_links)
+        self.assertEqual(list(self.app1.ud_links.keys()), app1_simple_case_links)
+
+    def test_PrivateLink_CustomEndpoint(self):
+        """
+        Adds a private rule to an app with custom endpoint.
+        Validates that the added rule was hidden and not exposed
+        :return: void
+        """
+        print()
+        print("====== TEST PRIVATE LINK -- CUSTOM ENDPOINT ======")
+
+        @private()
+        @self.app1.route("/test/hidden_rule/", endpoint="test_endpoint1")
+        def test_endpoint():
+            """
+            Test url_rule + view_function. Custom endpoint is used
+            :return: void
+            """
+            return
+
+        discover_urls(self.app1)
+        print("Collected Links: ", self.app1.ud_links)
+        self.assertEqual(list(self.app1.ud_links.keys()), app1_simple_case_links)
+
+    def test_PrivateBlueprint(self):
+        """
+        Privates a blueprint.
+        Registers a blueprint into an application.
+        Collects routes from url_map that gets mapped to name of a view_function or endpoint.
+        Validates result dictionary that gets stored directly in the flask application
+        :return: void
+        """
+        print()
+        print("====== TEST PRIVATE BLUEPRINT ======")
+
+        private(self.blueprint1)
+        self.app1.register_blueprint(self.blueprint1)
+
+        discover_urls(self.app1)
+        print("Collected Links: ", self.app1.ud_links)
+        self.assertEqual(list(self.app1.ud_links.keys()), app1_simple_case_links)
